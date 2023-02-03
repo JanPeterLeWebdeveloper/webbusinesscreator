8190cfe21e7fabf78e1f3a06b3610be8643a8e2c
jan peter <masteryourownway@gmail.com>
2023-02-03 18:30:15 +0900

    greetings from seoul

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@ class PagesController < ApplicationController
   # before_filter :authenticate_user!, only: %i[dashboard messages courses]
   # rails devise if user logged in show only pages
   def home
-    @tests = policy_scope(Test).last(3)
     @projects = policy_scope(Project).last(3)
     @disable_sidebar = true
   end
@@ -12,6 +11,7 @@ class PagesController < ApplicationController
   end
 
   def portfolio
+    @projects = policy_scope(Project)
   end
 
   def services
